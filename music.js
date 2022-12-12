78be8bb224011b4d17d2889fc9bfd02f34e6f404
stopTHATcat <119391891+Preramo@users.noreply.github.com>
2022-12-12 21:42:42 +0800

    Add files via upload
    Signed-off-by: stopTHATcat <119391891+Preramo@users.noreply.github.com>

---
+++ music.js
@@ -0,0 +1,45 @@
+var musicNode = document.getElementsByClassName('music')[0];
+var     mlist = document.getElementsByClassName('mlist')[0];
+var       lis = document.getElementsByTagName('li');
+var       len = lis.length;
+var  musicsrc =['music/Alanis Morissette - UR.mp3'
+   ,'music/Taylor Swift - the 1.mp3'
+   ,'music/Rosenfeld - I Want To.mp3'
+   ,'music/Prince; The Revolution - Kiss.mp3'
+   ];
+   musicNode.src = musicsrc[0];
+ for (var i = 0; i < len; i++) {//单击改变playmusic
+ 	(function(i){
+ 		lis[i].onclick =function(){        
+ 		musicNode.src = musicsrc[i];
+ 		musicNode.load();
+ 		musicNode.play();
+ 		for (var j= 0; j < len; j++) {
+ 			lis[j].className = '';
+ 		}
+ 		this.className = 'play';
+ 	}})(i);
+ }
+ musicNode.onended =function(){//音乐播放下一曲
+ 	 var ended = getPlay();
+     if (ended == 2) {//若为最后一曲则放第一曲
+      musicNode.src = musicsrc[0];
+      lis[0].className = 'play'
+       lis[ended].className = '';
+      musicNode.load();
+ 	  musicNode.play(); 
+     }else{ 
+      musicNode.src = musicsrc[ended + 1];
+      lis[ended + 1].className = 'play';
+      lis[ended].className = '';
+      musicNode.load();
+ 	  musicNode.play(); }
+     
+ }
+function getPlay(){//获取正在播放music的li
+     for (var i = 0; i < len; i++) {
+     	if (lis[i].getAttribute('class') == 'play') {
+     		return i
+     	}
+      }
+}
